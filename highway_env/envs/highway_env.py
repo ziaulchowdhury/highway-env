7f3913ef38402013beda477449b5a7c5c6fb4418
Ziaul Chowdhury <romel99@gmail.com>
2021-05-16 13:07:09 +0200

    Added reward (penalty) for changing lane

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -88,7 +88,8 @@ class HighwayEnv(AbstractEnv):
         reward = \
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
-            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
+            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1) \
+            + self.config["lane_change_reward"] * lane_change
         reward = utils.lmap(reward,
                           [self.config["collision_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
